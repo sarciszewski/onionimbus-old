cc14886aac4ed4c6e4574e61d57e275a88ec7d3c
scott <scott@ivoryfort>
2013-09-20 10:35:23 -0400

    Just pushing these changes to the public eye before taking a break

--- onionimbus_site/create_database.php
+++ onionimbus_site/create_database.php
@@ -1,4 +1,33 @@
 <?
 // Use argv, argc to determine if we're creating a sqlite database or a MySQL db
-// TODO: All of this
+/*
+ * CURRENT STATUS: Rethinking the approach here :(
+ */
+
+function usage() {
+  echo <<<EOUSAGE
+USAGE (one of the following formats):
+  php create_database.php sqlite [filename]
+  php create_database.php mysql username [databasename] [localhost]
+EOUSAGE;
+  die("\n");
+}
+if($argc < 2) {
+  usage();
+}
+$dbtype = array_shift($argv);
+switch($dbtype) {
+  case 'sqlite':
+      $dbfile = array_shift($argv);
+      if(!is_writeable($dbfile)) {
+        die("{$dbfile} is not writeable! Please chmod or chown it.\n");
+      }
+      $DB = new PDO("sqlite:{$dbfile}");
+    break;
+  case 'mysql':
+    break;
+  default:
+    usage();
+    break;
+}
 ?>
--- onionimbus_site/outsite_webroot/includes/global.php
+++ onionimbus_site/outsite_webroot/includes/global.php
@@ -1,56 +1,70 @@
 <?
 if(!defined('GLOBAL_LOADED')) {
-$SETTINGS =  ini_get('/var/onionimbus/settings.ini');
-define('SETTINGS_KEEP', serialize($SETTINGS));
-/* 
- * Just in case we get paranoid in the future, we can do this:
- * $SETTINGS = unserialize(SETTINGS_KEEP);
- */
-// If you have time, update your php.ini file and comment these lines out
-// You'll get better performance
-ini_set('session.cookie_httponly', true);
-ini_set('session.cookie_secure', true); # Comment this out if not using HTTPS
-ini_set('session.entropy_file', '/dev/urandom');
-ini_set('session.cache_expire', '43200');
-ini_set('session.cookie_lifetime', '43200');
-ini_set('session.entropy_length', '32'); # 256 bits
-ini_set('session.hash_function', 'sha256'); # 256 bits
-ini_set('session.hash_bits_per_character', '6'); # 43 characters
-if(!is_dir(ini_get('session.save_path'))) {
-  // Prevent session errors during server reboot
-  @mkdir(ini_get('session.save_path'));
-  @chmod(ini_get('session.save_path'), 0777);
-}
-if(!session_id()) session_start();
-if(empty($_SESSION['birth'])) {
-  if(!empty($_SESSION)) {
-    foreach($_SESSION as $i => $v) {
-      $_SESSION[$i] = openssl_random_pseudo_bytes(strlen($v));
-      unset($_SESSION[$i]);
-    }
+  $SETTINGS =  ini_get('/var/onionimbus/settings.ini');
+  define('SETTINGS_KEEP', serialize($SETTINGS));
+  /* 
+   * Just in case we get paranoid in the future, we can do this:
+   * $SETTINGS = unserialize(SETTINGS_KEEP);
+   */
+  // If you have time, update your php.ini file and comment these lines out
+  // You'll get better performance
+  ini_set('session.cookie_httponly', true);
+  ini_set('session.cookie_secure', true); # Comment this out if not using HTTPS
+  ini_set('session.entropy_file', '/dev/urandom');
+  ini_set('session.cache_expire', '43200');
+  ini_set('session.cookie_lifetime', '43200');
+  ini_set('session.entropy_length', '32'); # 256 bits
+  ini_set('session.hash_function', 'sha256'); # 256 bits
+  ini_set('session.hash_bits_per_character', '6'); # 43 characters
+  if(!is_dir(ini_get('session.save_path'))) {
+    // Prevent session errors during server reboot
+    @mkdir(ini_get('session.save_path'));
+    @chmod(ini_get('session.save_path'), 0777);
   }
-  $_SESSION['birth'] = time();
-  session_regenerate_id(true);
-} elseif(time() - $_SESSION['birth'] > 1800) {
-  // After 30 minutes, update session ID
-  $_SESSION['birthTime'] = time();
-  session_regenerate_id(false);
-  $_SESSION['birthTime'] = time()+1;
-}
-// Allows $HMAC['specificForm'] to store HMAC keys in $_SESSION. Laziness.
-if(empty($_SESSION['hmac'])) $_SESSION['hmac'] = array();
-$HMAC =& $_SESSION['hmac'];
-//error_reporting(E_ALL); ini_set('display_errors', 'On');
-include_once "HTMLPurifier.auto.php";
-$XSS = new HTMLPurifier();
-
-define('MAX_REC_DEPTH', 50); // Used in functions
-$jqueryver = '1.10.2'; // Load this version of jQuery everywhere!
-
-require_once $SETTINGS['includes'].'libscott.php';
-require_once $SETTINGS['includes'].'pbkdf2.php';
-# WE CAN USE SQLITE, MYSQL, ETC.
-$DB = new PDO('sqlite:/var/onionimbus/db/main.db');
-//$DB = new PDO('mysql:dbname=onionimbus;host=127.0.0.1', 'username', 'password');
+  if(!session_id()) session_start();
+  if(empty($_SESSION['birth'])) {
+    if(!empty($_SESSION)) {
+      foreach($_SESSION as $i => $v) {
+        $_SESSION[$i] = openssl_random_pseudo_bytes(strlen($v));
+        unset($_SESSION[$i]);
+      }
+    }
+    $_SESSION['birth'] = time();
+    session_regenerate_id(true);
+  } elseif(time() - $_SESSION['birth'] > 1800) {
+    // After 30 minutes, update session ID
+    $_SESSION['birthTime'] = time();
+    session_regenerate_id(false);
+    $_SESSION['birthTime'] = time()+1;
+  }
+  // Allows $HMAC['specificForm'] to store HMAC keys in $_SESSION. Laziness.
+  if(empty($_SESSION['hmac'])) $_SESSION['hmac'] = array();
+  $HMAC =& $_SESSION['hmac'];
+  //error_reporting(E_ALL); ini_set('display_errors', 'On');
+  include_once "HTMLPurifier.auto.php";
+  $XSS = new HTMLPurifier();
+  
+  define('MAX_REC_DEPTH', 50); // Used in functions
+  $jqueryver = '1.10.2'; // Load this version of jQuery everywhere!
+  
+  require_once $SETTINGS['includes'].'libscott.php';
+  require_once $SETTINGS['includes'].'pbkdf2.php';
+  # WE CAN USE SQLITE OR MYSQL
+  try {
+    switch($SETTINGS['db_type']) {
+      case 'sqlite':
+          $DB = new PDO('sqlite:'.$SETTINGS['root'].'main.db');
+        break;
+      case 'mysql':
+        $DB = new PDO('mysql:dbname='.$SETTINGS['db_name'].';host='.$SETTINGS['db_host'], 
+                $SETTINGS['db_user'], $SETTINGS['db_pass']);
+        break;
+      default:
+        die("Unsupported database type selected: ".stripXSS($SETTINGS['db_type']));
+        break;
+    }
+  } catch (PDOException $e) {
+    die("Connection failed: ".$e->getMessage());
+  }
   define('GLOBAL_LOADED', true);
 }
